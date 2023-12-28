4ea34338ca15216c1fade17b1ebfca8619e440e8
Jeferson <jefeleal363@gmail.com>
2023-12-28 18:51:48 -0300

    feat: aula-03

---
+++ aula-03.js
@@ -0,0 +1,10 @@
+const idade = 28;
+const nome = 'Julio';
+
+if(idade >= 18 && nome === 'Julio') {
+    podeEntrar = true;
+    console.log('ENTROU');
+} else {
+    console.log('NAO ENTRA!!!')
+}
+
