554d4062d1db57234c105ae3aa826c6e171e2826
Jeferson <jefeleal363@gmail.com>
2024-07-02 14:33:49 -0300

    feat: aula-08

---
+++ aula-08.js
@@ -0,0 +1,13 @@
+//const salario = '103';
+
+//console.log('salario: ', salario.toFixed(2));
+
+//console.log('salario: ', Number(salario).toFixed(2));
+
+
+const salario = '103';
+const salarioNumero = Number(salario);
+
+console.log('salario: ', salario);
+console.log('salario numero: ', salarioNumero);
+console.log('salario numero: ', salarioNumero.toFixed(2));
