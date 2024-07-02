493349c888c2218ad787f3481546f5f33968214d
Jeferson <jefeleal363@gmail.com>
2024-07-02 14:56:10 -0300

    feat: aula-09

---
+++ aula-09.js
@@ -0,0 +1,6 @@
+
+function soma(item1, item2) {
+    return item1 + item2;
+}
+
+console.log('SOMA: ', soma(5, 6));
--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const salario = '103';
-const salarioNumero = Number(salario);
-
-console.log('salario: ', salario);
-console.log('salario numero: ', salarioNumero);
-console.log('salario numero: ', salarioNumero.toFixed(2));
