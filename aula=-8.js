eb7beeb641c9bb759713a2927bad330e7d78f636
Jeferson <jefeleal363@gmail.com>
2024-08-09 20:17:18 -0300

    feat: aula-08

---
+++ aula=-8.js
@@ -0,0 +1,6 @@
+const salario = '1500';
+const salarioNumero = Number(salario);
+
+console.log('salario: ', salario) //string
+console.log('salarioNumero: ', salarioNumero) //numero
+console.log('salarioNumero: ', salarioNumero.toFixed(2)) //numero com casa decimal
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const salario = 1500;
+
+console.log('salario: ', salario.toFixed(2))
