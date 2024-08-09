8182a829ee465e734b58ec394fd5c29dc95eca20
Jeferson <jefeleal363@gmail.com>
2024-08-09 19:54:50 -0300

    feat: aula-07

---
+++ aula-07.js
@@ -0,0 +1,11 @@
+const idadeJulio = 18;
+const idadeJoao = 16;
+const idadeMaria = 17;
+
+const idades = [18, 16, 17, 15, 8, 90, 19, 27, 38];
+
+for (let i = 0; i < idades.length; i++) {
+    console.log('resultado:', idades[i]);
+}
+
+console.log('resultado: ',idades.length)
