dcef3adcc6bab4bcd18d0808e36102ae55ac4924
Jeferson <jefeleal363@gmail.com>
2024-08-09 11:02:32 -0300

    feat: aula-03

---
+++ aula-03.js
@@ -0,0 +1,10 @@
+const idade = 16;
+const nome = 'Alex Ferreira Santos Leal';
+let podeEntrar = false;
+
+if(idade >= 18 && nome === 'Alex Ferreira Santos Leal') {
+    console.log('Pode entrar');
+} else {
+    console.log('Não pode entrar');
+}
+
