e947f3c8bd57c392281affa62b4a23e56c8a1a09
Jeferson <jefeleal363@gmail.com>
2024-08-13 15:29:46 -0300

    feat: aula-36

---
+++ aula-36.js
@@ -0,0 +1,35 @@
+class Usuario {
+    email;
+    senha;
+    nome;
+
+    constructor(email, senha, nome) {
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+    }
+}
+
+class Administrador extends Usuario {
+    permissoes;
+
+    constructor(email, senha, nome, permissoes) {
+        super(email, senha, nome)
+        this.permissoes = permissoes;
+
+    }
+}
+
+class Comprador extends Usuario {
+    compras;
+
+}
+
+const admin = new Administrador('email@email.com', 'senha', 'Jose', [1, 6]);
+
+const comprador = new Comprador();
+comprador.senha = 'nova senha';
+
+console.log(admin.email);
+
+console.log(comprador);
