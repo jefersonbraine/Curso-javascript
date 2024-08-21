54d56da2149defeefbd971cdab44eb0ae090d474
Jeferson <jefeleal363@gmail.com>
2024-08-21 18:55:12 -0300

    feat: aula1 comentada

--- aula-01.js
+++ aula-01.js
@@ -1,5 +1,9 @@
+// Declara duas variáveis e atribui valores numéricos
 var x = 1;
 var y = 3;
+
+// Calcula a soma das duas variáveis e armazena o resultado em uma nova variável
 var soma = x + y;
 
-console.log(soma)
+// Imprime o resultado da soma no console
+console.log(soma);
